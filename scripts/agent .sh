4ecb01f98dbae563a573880a7762f5314c3b0615
Oloami Abisoye <olaomiabisoye@gmail.com>
2019-10-10 00:28:57 +0100

    all correct

--- scripts/agent .sh
+++ scripts/agent .sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum install ruby wget -y
+sudo yum install ruby wget httpd -y
 cd /home/ec2-user
 wget https://aws-codedeploy-eu-west-2.s3.amazonaws.com/latest/install
 chmod +x ./install
